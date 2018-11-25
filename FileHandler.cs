2a07f8d916c3c3d70bf98e9aaef40c55017e00f6
MichaJ <michajanssen93@gmail.com>
2018-11-26 00:14:27 +0100

    Test Round #1
    Test1
    - Testing out Json File Handling and Creations
       o Writing file no succes.

--- FileHandler.cs
+++ FileHandler.cs
@@ -2,25 +2,46 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ItemDB2
 {
     class FileHandler
     {
         Handler hlr;
+        
         public FileHandler()
         {
             hlr = new Handler();
         }
+        public void createJson(JsonLoginData jld)
+        {
+            List<JsonLoginData> _jld = new List<JsonLoginData>();
+            _jld.Add(jld);
+            string objjsonData = JsonConvert.SerializeObject(_jld.ToArray());
+            
+            if (!File.Exists(objjsonData))
+            {
+                try
+                {
+                    File.Create("LoginData.json", objjsonData);
+                }
+                catch (System.IO.FileNotFoundException ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                }
+            }
+        }
         public void writeToJson(JsonLoginData jld)
         {
             string objjsonData = JsonConvert.SerializeObject(jld);
-            string fileType = ""
-            System.IO.File.WriteAllText(objjsonData);
+            if (File.Exists(objjsonData)){
+                File.WriteAllLines("LoginData.json", objjsonData.ToArray());
+            };
         }
-
     }
-}
+};
